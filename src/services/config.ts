fc22f2ee77286cf644ea827860cb248de0a00251
servocams <128822922+servocams@users.noreply.github.com>
2023-03-24 12:26:46 -0300

    Update config.ts
    fix

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,3 +1,1 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
-
-let medusaUrl = "https://servocamsbackend.herokuapp.com/"
+const medusaUrl = "https://servocamsbackend.herokuapp.com/"
